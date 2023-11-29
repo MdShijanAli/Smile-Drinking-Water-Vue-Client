3cefd6ea8b2d6b4ee5244ecdf5f400bbd6c329b8
MdShijanAli <freelancershijan@gmail.com>
2023-11-29 12:51:44 +0600

    Header and Footer Complete and Basic Structure DOne

---
+++ src/components/HomeSliderSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeSliderSection",
+
+  data() {
+    return {
+      msg: "Home Page SLider Section"
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
